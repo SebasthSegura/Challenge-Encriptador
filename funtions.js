612e51ce60f4c1d7e8c84b97bd0a644044040ddb
sebasth <sebasthsegura05@gmail.com>
2024-08-25 22:17:58 -0500

    creacio de la funcion para el boton de desencriptar

--- funtions.js
+++ funtions.js
@@ -8,6 +8,7 @@ function btnEncrip() {
     const encryptedText = encriptation(textToEncrypt);
     outputText.textContent = encryptedText;
     textInput.value = "";
+    outputText.style.backgroundImage = "none"
 }
 
 //creamos la funcion respectiva para encriptar los tatos capturados
@@ -27,3 +28,28 @@ function encriptation(stringEncriptada) {
     return stringEncriptada;
 }
 
+//creamos la funcion que va a llamar el boton de desencriptar
+function btnunEncrip() {
+    const textToEncrypt = textInput.value;
+    const encryptedText = unEncriptation(textToEncrypt);
+    outputText.textContent = encryptedText;
+    textInput.value = "";
+    outputText.style.backgroundImage = "none"
+}
+
+//ahora creamos la funcion para desencriptar a partir de la funcion de enriptar
+function unEncriptation(stringunEncriptada) {
+    
+    //creamos una matriz con los datos de encriptacion referentes a cada letra que se usaron
+    let datos = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringunEncriptada = stringunEncriptada.toLowerCase()
+    
+    //creamos un ciclo repetitivo for para recorrer la matriz
+    for(let i = 0; i < datos.length; i++){
+        if (stringunEncriptada.includes(datos[i][1])) {
+            stringunEncriptada = stringunEncriptada.replaceAll(datos[i][1], datos[i][0])
+        }
+    }
+    //retornamos los hechos
+    return stringunEncriptada;
+}
